8f39f1679470238a1a5ddcd3829bc107c14ea4bc
shadman <shadmansutshob@gmail.com>
2021-01-12 19:57:40 +0600

    first commit

---
+++ app/src/main/java/com/example/fragmentdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.fragmentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.ButtonId);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                BlankFragment blankFragment = new BlankFragment();
+                fragmentTransaction.add(R.id.wrapperId,blankFragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+    }
+}
